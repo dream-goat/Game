3894584a1bcaa3742d32ca194a386dc4eb3d1c08
altezy <49876941+altezy@users.noreply.github.com>
2019-04-22 19:37:39 +0700

    Menu of the game

---
+++ Menu/Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() //это метод кнопки Run!(Play)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //GetActiveScene берет индекс уровня, на котором сейчас игрок, и запускает следующий в очереди
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
